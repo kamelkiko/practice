abc31cfe57d0ccc05bd43aa6d3b81882492f36db
Kamel <kikokeko10@gmail.com>
2025-08-17 02:32:54 +0300

    init practice code

--- src/main/kotlin/com/kamel/practice/util/GameExceptionHandler.kt
+++ src/main/kotlin/com/kamel/practice/util/GameExceptionHandler.kt
@@ -1,6 +1,5 @@
 package com.kamel.practice.util
 
-import com.kamel.practice.repo.ServerResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
