a7d138ddcd64ada3a23504acb15f6322ea23624e
Kamel <kikokeko10@gmail.com>
2025-08-17 02:28:04 +0300

    init practice project

--- src/main/kotlin/com/kamel/practice/service/Card.kt
+++
@@ -1,3 +0,0 @@
-package com.kamel.practice.service
-
-data class Card(val rank: String, val suit: String)
--- src/main/kotlin/com/kamel/practice/service/Play.kt
+++
@@ -1,3 +0,0 @@
-package com.kamel.practice.service
-
-data class Play(val playerId: String, val facedown: List<Card>, val claimedRank: String)
