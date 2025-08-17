565990053ee37a24f699297e4b16e3ebe7c771c2
Kamel <kikokeko10@gmail.com>
2025-08-17 03:15:18 +0300

    finish controller

---
+++ src/main/kotlin/com/kamel/practice/api/controller/CarController.kt
@@ -0,0 +1,157 @@
+package com.kamel.practice.api.controller
+
+import com.kamel.practice.api.dto.*
+import com.kamel.practice.domain.exception.CarNotFoundException
+import com.kamel.practice.domain.service.CarService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/cars")
+class CarController(
+    private val carService: CarService,
+) {
+    @GetMapping
+    fun getAllCars(): ServerResponse<List<CarDto>> {
+        val cars = carService.getAllCars()
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/code/{code}")
+    fun getCarByCode(@PathVariable code: String): ServerResponse<CarDto> {
+        val car = carService.getCarByCode(code)
+            ?: throw CarNotFoundException("Car with code $code not found.")
+        return sendSuccessResponse(
+            data = car.toDto(),
+            successMessage = "Car retrieved successfully."
+        )
+    }
+
+    @GetMapping("/{id}")
+    fun getCarById(@PathVariable id: String): ServerResponse<CarDto> {
+        val car = carService.getCarById(id)
+            .orElseThrow { CarNotFoundException("Car with id $id not found.") }
+        return sendSuccessResponse(
+            data = car.toDto(),
+            successMessage = "Car retrieved successfully."
+        )
+    }
+
+    @GetMapping("/brand/{brand}")
+    fun getCarByBrand(@PathVariable brand: String): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarByBrand(brand)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/model/{model}")
+    fun getCarByModel(@PathVariable model: String): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarByModel(model)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/year/{year}")
+    fun getCarByYear(@PathVariable year: Int): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarByYear(year)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/price/{price}")
+    fun getCarByPrice(@PathVariable price: Double): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarByPrice(price)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/color/{color}")
+    fun getCarByColor(@PathVariable color: String): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarByColor(color)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/brand/{brand}/model/{model}")
+    fun getCarByBrandAndModel(
+        @PathVariable brand: String,
+        @PathVariable model: String
+    ): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarByBrandAndModel(brand, model)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/price-range")
+    fun getCarsByPriceRange(
+        @RequestParam("minPrice", required = true, defaultValue = "0.0") minPrice: Double,
+        @RequestParam("maxPrice", required = true, defaultValue = "0.0") maxPrice: Double
+    ): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarsByPriceRange(minPrice, maxPrice)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @GetMapping("/year-range")
+    fun getCarsByYearRange(
+        @RequestParam("minYear", required = true, defaultValue = "0") minYear: Int,
+        @RequestParam("maxYear", required = true, defaultValue = "0") maxYear: Int
+    ): ServerResponse<List<CarDto>> {
+        val cars = carService.getCarsByYearRange(minYear, maxYear)
+        return sendSuccessResponse(
+            data = cars.map { it.toDto() },
+            successMessage = "Cars retrieved successfully."
+        )
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun saveCar(@RequestBody carDto: CarDto): ServerResponse<CarDto> {
+        val car = carService.saveCar(carDto.toEntity())
+        return sendSuccessResponse(
+            data = car.toDto(),
+            successMessage = "Car saved successfully.",
+            code = HttpStatus.CREATED.value()
+        )
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    fun updateCar(
+        @PathVariable id: String,
+        @RequestBody carDto: CarDto
+    ): ServerResponse<CarDto> {
+        val updatedCar = carService.updateCar(id, carDto.toEntity())
+        return sendSuccessResponse(
+            data = updatedCar.toDto(),
+            successMessage = "Car updated successfully.",
+            code = HttpStatus.ACCEPTED.value()
+        )
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCarById(@PathVariable id: String): ServerResponse<Unit> {
+        carService.deleteCarById(id)
+        return sendSuccessResponse(
+            data = Unit,
+            successMessage = "Car deleted successfully."
+        )
+    }
+}
--- src/main/kotlin/com/kamel/practice/api/dto/CarDto.kt
+++ src/main/kotlin/com/kamel/practice/api/dto/CarDto.kt
@@ -4,6 +4,7 @@ import com.kamel.practice.data.model.Car
 import java.util.*
 
 data class CarDto(
+    val id: String? = null,
     val code: String? = null,
     val brand: String,
     val model: String,
@@ -13,9 +14,22 @@ data class CarDto(
     val pictureUrl: String? = null,
 )
 
-fun CarDto.toCar(): Car {
+fun CarDto.toEntity(): Car {
     return Car(
         code = code ?: createRandomCode(model),
+        brand = brand,
+        model = model,
+        year = year,
+        price = price,
+        color = color,
+        pictureUrl = pictureUrl
+    )
+}
+
+fun Car.toDto(): CarDto {
+    return CarDto(
+        id = id.toHexString(),
+        code = code,
         brand = brand,
         model = model,
         year = year,
