bce7fe4f92da730c74d80138b28a6801481ec2c0
Kamel <kikokeko10@gmail.com>
2025-08-20 14:31:11 +0300

    init chat module

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
