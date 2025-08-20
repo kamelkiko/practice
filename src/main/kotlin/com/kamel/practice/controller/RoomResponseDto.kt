4de81774f542f82cfd6847ae3bb85ee2cc342f2a
Kamel <kikokeko10@gmail.com>
2025-08-20 14:50:00 +0300

    init models and mappers

---
+++ src/main/kotlin/com/kamel/practice/controller/RoomResponseDto.kt
@@ -0,0 +1,12 @@
+package com.kamel.practice.controller
+
+import java.time.LocalDateTime
+
+data class RoomResponseDto(
+    val id: String,
+    val name: String,
+    val description: String?,
+    val activeUsers: Int,
+    val createdAt: LocalDateTime,
+    val isActive: Boolean,
+)
