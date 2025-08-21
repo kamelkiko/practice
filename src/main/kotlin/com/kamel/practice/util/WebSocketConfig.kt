8906e0e43cf3b9609cfb920052fe3e13f686dbf5
Kamel <kikokeko10@gmail.com>
2025-08-21 15:06:33 +0300

    add ws config

---
+++ src/main/kotlin/com/kamel/practice/util/WebSocketConfig.kt
@@ -0,0 +1,35 @@
+package com.kamel.practice.util
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/app")
+            .enableSimpleBroker("/topic", "/queue")
+            .setHeartbeatValue(longArrayOf(25000, 25000)) // Heartbeat interval for both client and server
+            .setTaskScheduler(heartbeatTaskScheduler())
+
+        registry.setUserDestinationPrefix("/user")
+    }
+
+    private fun heartbeatTaskScheduler() = ThreadPoolTaskScheduler().apply {
+        poolSize = 1
+        setThreadNamePrefix("websocket-heartbeat-")
+        initialize()
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+            .setHeartbeatTime(25000)
+            .setDisconnectDelay(30000)
+    }
+}
