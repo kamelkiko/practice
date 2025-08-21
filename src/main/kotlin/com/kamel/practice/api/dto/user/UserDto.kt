8b895fe0a97dcc4b15e3ee10e4a5f0317d5e89d6
Kamel <kikokeko10@gmail.com>
2025-08-21 16:47:27 +0300

    handle not null dto

--- src/main/kotlin/com/kamel/practice/api/dto/user/UserDto.kt
+++ src/main/kotlin/com/kamel/practice/api/dto/user/UserDto.kt
@@ -20,6 +20,6 @@ fun User.toDto(): UserDto {
         email = email,
         createdAt = LocalDateTime.ofInstant(createdAt, ZoneId.systemDefault()),
         status = status,
-        profilePictureUrl = profilePictureUrl
+        profilePictureUrl = "images/$profilePictureUrl"
     )
 }
