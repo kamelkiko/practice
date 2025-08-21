e44920a884372b631dbeff5b034af7726c41cb60
Kamel <kikokeko10@gmail.com>
2025-08-21 14:35:51 +0300

    add user document for db

---
+++ src/main/kotlin/com/kamel/practice/data/User.kt
@@ -0,0 +1,24 @@
+package com.kamel.practice.data
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("User")
+data class User(
+    @Id
+    val id: ObjectId = ObjectId.get(),
+    val code: String,
+    val username: String,
+    val email: String,
+    val password: String,
+    val createdAt: Long = System.currentTimeMillis(),
+    val profilePictureUrl: String? = null,
+    val status: Status,
+    //val phoneNumber: String? = null,
+    //val bio: String? = null,
+)
+
+enum class Status {
+    ONLINE, OFFLINE
+}
