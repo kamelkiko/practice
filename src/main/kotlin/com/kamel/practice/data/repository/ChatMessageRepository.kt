e3db8fa28e90649e2c9c5a3a52f318bc4fd46911
Kamel <kikokeko10@gmail.com>
2025-08-21 18:28:07 +0300

    finish chat room and chat message repository

---
+++ src/main/kotlin/com/kamel/practice/data/repository/ChatMessageRepository.kt
@@ -0,0 +1,12 @@
+package com.kamel.practice.data.repository
+
+import com.kamel.practice.data.model.ChatMessage
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ChatMessageRepository : MongoRepository<ChatMessage, ObjectId> {
+    fun findByRoomIdOrderByTimestampDesc(roomId: String): List<ChatMessage>
+    fun findByRoomIdAndSenderIdOrderByTimestampDesc(roomId: String, senderId: String): List<ChatMessage>
+}
---
+++ src/main/kotlin/com/kamel/practice/data/repository/ChatRoomRepository.kt
@@ -0,0 +1,14 @@
+package com.kamel.practice.data.repository
+
+import com.kamel.practice.data.model.ChatRoom
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ChatRoomRepository : MongoRepository<ChatRoom, ObjectId> {
+    fun findByCreatedByOrderByCreatedAtDesc(createdBy: String): List<ChatRoom>
+    fun findByIdAndCreatedBy(id: ObjectId, createdBy: String): ChatRoom?
+    fun findAllByIsActiveOrderByCreatedAtDesc(isActive: Boolean): List<ChatRoom>
+    fun findAllByIdOrderByCreatedAtDesc(id: ObjectId): List<ChatRoom>
+}
