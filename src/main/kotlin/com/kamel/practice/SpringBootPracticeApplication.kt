fa0c1afb172cf3a58ae7d51e375172c5e35172cf
Kamel <kikokeko10@gmail.com>
2025-08-22 20:28:14 +0300

    add cors

--- src/main/kotlin/com/kamel/practice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/kamel/practice/SpringBootPracticeApplication.kt
@@ -2,9 +2,16 @@ package com.kamel.practice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class SpringBootPracticeApplication
+class SpringBootPracticeApplication : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        super.addCorsMappings(registry)
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringBootPracticeApplication>(*args)
