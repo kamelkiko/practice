ceb749cf96cddb4995730376077602d769481934
Kamel <kikokeko10@gmail.com>
2025-08-22 19:15:17 +0300

    handle exit user from room

--- src/main/kotlin/com/kamel/practice/config/WebSocketEventHandler.kt
+++ src/main/kotlin/com/kamel/practice/config/WebSocketEventHandler.kt
@@ -1,17 +1,25 @@
 package com.kamel.practice.config
 
+import com.kamel.practice.api.dto.message.ChatMessageResponseDto
+import com.kamel.practice.data.model.ChatMessage
 import com.kamel.practice.domain.service.room.RoomUserService
+import com.kamel.practice.domain.service.user.UserService
 import org.springframework.context.event.EventListener
+import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.messaging.simp.broker.BrokerAvailabilityEvent
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.messaging.SessionConnectEvent
 import org.springframework.web.socket.messaging.SessionDisconnectEvent
 import org.springframework.web.socket.messaging.SessionSubscribeEvent
+import java.time.LocalDateTime
+
 
 @Component
 class WebSocketEventHandler(
     private val roomUserService: RoomUserService,
+    private val userService: UserService,
+    private val messagingTemplate: SimpMessageSendingOperations,
 ) {
     @EventListener
     fun handleWebSocketConnectListener(event: SessionConnectEvent) {
@@ -19,11 +27,26 @@ class WebSocketEventHandler(
     }
 
     @EventListener
-    fun handleWebSocketDisconnectListener(event: SessionDisconnectEvent, headerAccessor: StompHeaderAccessor) {
+    fun handleWebSocketDisconnectListener(event: SessionDisconnectEvent) {
+        val headerAccessor: StompHeaderAccessor = StompHeaderAccessor.wrap(event.message)
         val userId = headerAccessor.sessionAttributes?.get("userId") as String?
         val roomId = headerAccessor.sessionAttributes?.get("roomId") as String?
         if (userId != null && roomId != null) {
             roomUserService.removeUserFromRoom(roomId, userId)
+            val user = userService.getUserById(userId)
+            messagingTemplate.convertAndSend(
+                "/topic/room",
+                ChatMessageResponseDto(
+                    roomId = roomId,
+                    senderId = userId,
+                    content = "${user.username} has left the room",
+                    timestamp = LocalDateTime.now(),
+                    messageType = ChatMessage.MessageType.LEAVE,
+                    senderUsername = user.username,
+                    senderProfilePictureUrl = user.profilePictureUrl,
+                    id = roomId
+                )
+            )
         }
         println("Client disconnected: ${event.message}")
     }
